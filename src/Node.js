eb6c3a307b412d6c64a0949a29c2f2a0a5ae8949
XXAssDestroyerXX <72551615+XXAssDestroyerXX@users.noreply.github.com>
2021-10-25 15:13:30 +0200

    dokończenie zad

--- src/Node.js
+++ src/Node.js
@@ -1,34 +1,38 @@
 //Serwer
-
 const express = require('express');
-const path = require('path')
-const path = require('hbs');
-const hbs = require('hbs');
-
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+var url = "mongodb+srv://Kacper:<Minecraft123>@cluster0.hndhl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const app = express();
+let db;
+let studentCollection;
 
-app.set('views', './src/views');
-hbs.registerPartials(path.join(__dirname, 'view/partials'));
-app.set('view engine', 'hbs');
-
-app.get('/', (req, res) => {
-  res.send("index", {});
-});
-
-app.get('/', (req, res) => {
-  res.send("Strona 2");
-});
-
-app.get('/3', (req, res) => {
-  res.send("Strona 3");
-});
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    db = client.db('Cluster0')
+    studentCollection = db.collection('studenci')
+  })
+app.listen(5000, function () {
+  console.log('5000')
+})
 
 app.get('/', (req, res) => {
-  res.send("Strona 4");
-});
+  res.sendFile(__dirname + '/index.html')
+  db.collection('studenci').find().toArray()
+    .then(results => {
+      console.log(results)
+    })
+    .catch(error => console.error(error))
+})
 
-app.listen(5000, () => {
-  console.log("Serwer działa")
+app.use(bodyParser.urlencoded({ extended: true }))
+app.post('studenci', (req, res) => {
 
-
-});
+  studentCollection.insertOne(req.body)
+    .then(result => {
+      res.redirect('/')
+      console.log(result)
+    })
+    .catch(error => console.error(error))
+})
